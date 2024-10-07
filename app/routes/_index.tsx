c3815f27ea7faf9acc9d67f6f06e31967ce0b143
bhaskarguthula <bhaskarguthula1674@gmail.com>
2024-10-07 15:23:24 +0530

    added fire emoji

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const IndexRoute = () => {
   return (
-    <div className='text-[3rem] text-center font-semibold h-[100dvh] w-full grid place-content-center'>Comming Soon</div>
+    <div className='text-[3rem] text-center font-semibold h-[100dvh] w-full grid place-content-center'>Comming Soon 🔥</div>
   )
 }
 
